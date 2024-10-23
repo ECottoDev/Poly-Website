92f2632da68dd0258d86e18d48a20ac4173b7e17
Ed Towers <edtowers@Eds-MacBook-Pro.local>
2024-10-23 15:21:28 -0400

    final touches

--- frontend/containers/awardListEditor/AwardListEditor.js
+++ frontend/containers/awardListEditor/AwardListEditor.js
@@ -9,18 +9,18 @@ export class AwardListEditor {
         this.awards = new EditableAwardArray(this.parentProps, awards);
         this.apply = apply;
         this.close = close;
-        this.view = addClasses(createPillBox(), 'articleListEditor_view');
+        this.view = addClasses(createPillBox(), 'awardListEditor_view');
         this.setView();
     }
     setView() {
         appendChildren(this.view, [
-            addClasses(createHeadingText('Editar Premios'), 'articleListEditor_heading'),
-            appendChildren(addClasses(createElementContainer(), 'articleListEditor_articleContainer'), [
+            addClasses(createHeadingText('Editar Premios'), 'awardListEditor_heading'),
+            appendChildren(addClasses(createElementContainer(), 'awardListEditor_articleContainer'), [
                 this.awards.view,
             ]),
-            addEvent(addClasses(createButton('Añadir premio'), 'articleListEditor_addButton'), () => { this.awards.addInput() }),
-            addEvent(addClasses(createButton('Aplicar'), 'articleListEditor_applyButton'), async () => { await updateAwardData(this.professorData.fullName, this.awards.getUpdatedArray()); this.apply() }),
-            addEvent(addClasses(createButton('Cancelar'), 'articleListEditor_closeButton'), () => { this.close() })
+            addEvent(addClasses(createButton('Añadir premio'), 'awardListEditor_addButton'), () => { this.awards.addInput() }),
+            addEvent(addClasses(createButton('Aplicar'), 'awardListEditor_applyButton'), async () => { await updateAwardData(this.professorData.fullName, this.awards.getUpdatedArray()); this.apply() }),
+            addEvent(addClasses(createButton('Cancelar'), 'awardListEditor_closeButton'), () => { this.close() })
         ])
     }
 }
