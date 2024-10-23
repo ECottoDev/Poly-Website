2fcdc7d86822e88f0d2cb4878e718bcf619edceb
Ed Towers <edtowers@Eds-MacBook-Pro.local>
2024-10-23 14:31:31 -0400

    final touches

--- frontend/containers/professorBiographyEdit/ProfessorBiographyEdit.js
+++ frontend/containers/professorBiographyEdit/ProfessorBiographyEdit.js
@@ -137,7 +137,7 @@ export class ProfessorBiographyEdit {
                     this.applyChanges();
                     delayExecution(async () => { this.close() }, 1000)
                 }),
-                addEvent(addClasses(createButton('Cancelar'), 'professorBiographyEdit_cancelEdit'), () => { detachChildren(this.view); removeClasses(this.view, 'professorBiographyEdit_view--edit'); this.setView(); detachChildren(this.container); this.biographyView() }),
+                addEvent(addClasses(createButton('Cancelar'), 'professorBiographyEdit_cancelEdit'), () => { removeClasses(this.view, 'professorBiographyEdit_view--edit'); detachChildren(this.view); this.setView(); detachChildren(this.container); this.biographyView() }),
                 addEvent(addClasses(createButton('Eliminar Profesor'), 'professorBiographyEdit_closeButton'), () => {
                     deleteProfessorData(this.professorData.fullName);
                     removeImage(this.professorData.fullName);
